752cbb2891822b043197def0223810c9150806a9
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2023-11-22 16:01:51 +0530

    removed fields from upload

--- src/collections/Upload.ts
+++ src/collections/Upload.ts
@@ -5,10 +5,41 @@ const Upload: CollectionConfig = {
   access: {
     read: () => true,
   },
-  upload:true,
-  labels:{
-    singular:'upload',
-    plural:'uploads'
+  upload: {
+    staticURL: '/upload',
+    staticDir: 'upload',
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        width: 768,
+        height: 1024,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 1024,
+        // By specifying `undefined` or leaving a height undefined,
+        // the image will be sized to a certain width,
+        // but it will retain its original aspect ratio
+        // and calculate a height automatically.
+        height: undefined,
+        position: 'centre',
+      },
+    ],
+    adminThumbnail: 'thumbnail',
+    mimeTypes: ['image/*'],
   },
+  fields: [
+    {
+      name: 'alt',
+      type: 'text',
+    },
+  ],
 }
 export default Upload
